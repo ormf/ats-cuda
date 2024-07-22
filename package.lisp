2a65a26eed1d981c03b776d50ac4266cc57e71f7
ormf <junk14@selma.hfmdk-frankfurt.de>
2024-07-22 12:13:26 +0200

    removed ats-cuda-display

--- package.lisp
+++ package.lisp
@@ -25,30 +25,3 @@
    #:ats-sound-amp-av #:ats-sound-dur #:ats-sound-time #:ats-sound-frq #:ats-sound-amp
    #:ats-sound-pha #:ats-sound-energy #:ats-sound-band-energy
    #:ats-save #:ats-load #:*ats-file-dir*))
-
-(defpackage #:ats-cuda-display
-  (:use :cl :incudine :cudere-clm :sb-loop :ieee-floats :alexandria
-;;;        :ats-cuda
-   :clog-dsp-widgets :cl-refs :clog :clog-dsp-widgets
-;;;        :de.finnendahl.binary-data :de.finnendahl.binary-data.common-datatypes
-        )
-  (:shadowing-import-from :clog
-   :run :rotate)
-  (:shadowing-import-from :ats-cuda
-   #:ats-sound-frames #:ats-sound-name #:ats-sound-sampling-rate #:ats-sound-frame-size
-   #:ats-sound-window-size #:ats-sound-partials #:ats-sound-frames #:ats-sound-bands
-   #:ats-sound-optimized #:ats-sound-ampmax #:ats-sound-frqmax #:ats-sound-frq-av
-   #:ats-sound-amp-av #:ats-sound-dur #:ats-sound-time #:ats-sound-frq #:ats-sound-amp
-   #:ats-sound-pha #:ats-sound-energy #:ats-sound-band-energy
-   #:ats->svg)
-  (:shadowing-import-from :incudine
-   :play :scale-envelope :normalize-envelope)
-  (:shadowing-import-from :incudine.util
-   :sample)
-  (:shadowing-import-from :cudere-clm
-   :*debug*)
-  (:export
-   #:ats-player-node-id #:ats-sound #:ats-fmod #:ats-amod #:ats-bw #:ats-x
-   #:ats-shift-x #:ats-width #:ats-idx #:ats-data #:ats-crosshairs #:ats-res-balance
-   #:ats-mousepos #:ats-scale #:ats-play #:data-watch #:play-watch #:pos-watch
-   #:ats->browser #:ats-display #:ats-display-init #:restore-tables))
