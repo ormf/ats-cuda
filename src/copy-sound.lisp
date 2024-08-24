547a65e20368ebe744e43d56d8f7fe3d53d0ece4
ormf <junk14@selma.hfmdk-frankfurt.de>
2024-08-24 14:54:26 +0200

    replaced copy-ats-data-array with copy-array

--- src/copy-sound.lisp
+++ src/copy-sound.lisp
@@ -15,7 +15,7 @@ copy-ats-data-array <array>
 copy an array of arrays
 "
   (let* ((len (first (array-dimensions array)))
-         (new (make-array len :element-type '(array double-float))))
+         (new (make-array len :element-type 'array)))
     (do ((i 0 (1+ i)))
         ((= i len) new)
       (setf (aref new i) (copy-seq (aref array i))))))	
@@ -23,33 +23,33 @@ copy an array of arrays
 ;;; copy of a sound
 (defun copy-ats-sound (sound &optional (new-sound nil))
   
-"
+  "
 copy-sound <sound> &optional [new-sound]
 copies <sound> into [new-sound] (def. <sound>-copy)
 "
   
-(let ((name (if new-sound (string new-sound) 
-		(concatenate 'string (ats-sound-name sound) "-copy"))))
-  (pushnew name *ats-sounds* :test #'equal)
-  (set (read-from-string name)
-       (make-ats-sound 
-	:name name
-	:sampling-rate (ats-sound-sampling-rate sound)
-	:frame-size (ats-sound-frame-size sound)
-	:window-size (ats-sound-window-size sound)
-	:partials (ats-sound-partials sound)
-	:frames (ats-sound-frames sound)
-	:bands (if (ats-sound-bands sound)(copy-seq (ats-sound-bands sound)))
-	:optimized (ats-sound-optimized sound)
-	:ampmax (ats-sound-ampmax sound)
-	:frqmax (ats-sound-frqmax sound)
-	:frq-av (copy-seq (ats-sound-frq-av sound))
-	:amp-av (copy-seq (ats-sound-amp-av sound))
-	:dur (ats-sound-dur sound)
-	:time (copy-ats-data-array (ats-sound-time sound))
-	:frq (copy-ats-data-array (ats-sound-frq sound))
-	:amp (copy-ats-data-array (ats-sound-amp sound)) 
-	:pha (if (ats-sound-pha sound) (copy-ats-data-array (ats-sound-pha sound)) nil)
-	:energy (if (ats-sound-energy sound)(copy-ats-data-array (ats-sound-energy sound)) nil)
-	:band-energy (if (ats-sound-band-energy sound)(copy-ats-data-array (ats-sound-band-energy sound)) nil)))))
+  (let ((name (if new-sound (string new-sound) 
+		  (concatenate 'string (ats-sound-name sound) "-copy"))))
+    (pushnew name *ats-sounds* :test #'equal)
+    (set (read-from-string name)
+         (make-ats-sound 
+	  :name name
+	  :sampling-rate (ats-sound-sampling-rate sound)
+	  :frame-size (ats-sound-frame-size sound)
+	  :window-size (ats-sound-window-size sound)
+	  :partials (ats-sound-partials sound)
+	  :frames (ats-sound-frames sound)
+	  :bands (if (ats-sound-bands sound)(copy-seq (ats-sound-bands sound)))
+	  :optimized (ats-sound-optimized sound)
+	  :ampmax (ats-sound-ampmax sound)
+	  :frqmax (ats-sound-frqmax sound)
+	  :frq-av (copy-seq (ats-sound-frq-av sound))
+	  :amp-av (copy-seq (ats-sound-amp-av sound))
+	  :dur (ats-sound-dur sound)
+	  :time (copy-array (ats-sound-time sound))
+	  :frq (copy-array (ats-sound-frq sound))
+	  :amp (copy-array (ats-sound-amp sound)) 
+	  :pha (if (ats-sound-pha sound) (copy-array (ats-sound-pha sound)) nil)
+	  :energy (if (ats-sound-energy sound)(copy-array (ats-sound-energy sound)) nil)
+	  :band-energy (if (ats-sound-band-energy sound)(copy-array (ats-sound-band-energy sound)) nil)))))
 
